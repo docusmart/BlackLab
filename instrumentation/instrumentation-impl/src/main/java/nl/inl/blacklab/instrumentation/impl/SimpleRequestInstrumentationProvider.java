5d60bcf799b82e195a10b4e5c727d5cce214875e
Esteban Ginez <175813+eginez@users.noreply.github.com>
2021-11-22 09:49:13 -0800

    swithiching headers to friendlier names

--- instrumentation/instrumentation-impl/src/main/java/nl/inl/blacklab/instrumentation/impl/SimpleRequestInstrumentationProvider.java
+++ instrumentation/instrumentation-impl/src/main/java/nl/inl/blacklab/instrumentation/impl/SimpleRequestInstrumentationProvider.java
@@ -10,9 +10,9 @@ import java.util.Optional;
 import java.util.UUID;
 
 public class SimpleRequestInstrumentationProvider implements RequestInstrumentationProvider {
-    private static final String ANN_REQUEST_ID_HEADER_NAME = "X-Request-ID";
-    private static final String ANN_RULE_ID_HEADER_NAME = "X-Ann-Rule-ID";
-    private static final String ANN_DOC_COUNT_HEADER = "X-Ann-Doc-Count";
+    private static final String REQUEST_ID = "X-Request-ID";
+    private static final String QUERY_ID = "X-Query-ID";
+    private static final String DOC_COUNT = "X-Doc-Count";
 
     @Override
     public Optional<String> getRequestID(HttpServletRequest request) {
@@ -21,28 +21,28 @@ public class SimpleRequestInstrumentationProvider implements RequestInstrumentat
 
     @Override
     public Map<String, String> getRequestMetadata(HttpServletRequest request) {
-        String docCount = request.getHeader(ANN_DOC_COUNT_HEADER);
+        String docCount = request.getHeader(DOC_COUNT);
         Map<String, String> metadata = new HashMap<>();
         metadata.put("inputDocCount", docCount);
         return metadata;
     }
 
     private String buildRequestId(HttpServletRequest request) {
-        String reqId = getRuleId(request).orElse(
+        String reqId = getQueryId(request).orElse(
             Base64.getUrlEncoder().encodeToString(UUID.randomUUID().toString().getBytes()));
-        return String.format("%s/%s", getAnnRequestId(request), reqId);
+        return String.format("%s/%s", getRequestId(request), reqId);
     }
 
-    protected String getAnnRequestId(HttpServletRequest request) {
-        String requestId = request.getHeader(ANN_REQUEST_ID_HEADER_NAME);
+    protected String getRequestId(HttpServletRequest request) {
+        String requestId = request.getHeader(SimpleRequestInstrumentationProvider.REQUEST_ID);
         if (requestId == null) {
             requestId = "unknown";
         }
         return  requestId;
     }
 
-    private Optional<String> getRuleId(HttpServletRequest request) {
-        String ruleId = request.getHeader(ANN_RULE_ID_HEADER_NAME);
-        return Optional.ofNullable(ruleId);
+    private Optional<String> getQueryId(HttpServletRequest request) {
+        String queryId = request.getHeader(SimpleRequestInstrumentationProvider.QUERY_ID);
+        return Optional.ofNullable(queryId);
     }
 }
