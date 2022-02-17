eb075ccf6e7902886a95016d7d1a4bec79e3d1a9
Esteban Ginez <175813+eginez@users.noreply.github.com>
2022-02-17 12:09:15 -0800

    Fixes inl tests

--- test/test/constants.js
+++ test/test/constants.js
@@ -1,11 +1,13 @@
 const SERVER_URL = process.env.APP_URL || "http://localhost:8080/blacklab-server";
 const BLACKLAB_USER = process.env.BLACKLAB_USER || "user";
 const BLACKLAB_PASSWORD = process.env.BLACKLAB_USER || "";
-const DEFAULT_WINDOW_SIZE = 50;
+const DEFAULT_WINDOW_SIZE = parseInt(process.env.BLACKLAB_DEFAULT_WINDOW_SIZE) || 50;
+const SHOULD_HAVE_CONTEXT = process.env.RESPONSE_SHOULD_HAVE_CONTEXT || false;
 
 module.exports = {
     SERVER_URL,
     DEFAULT_WINDOW_SIZE,
     BLACKLAB_USER,
-    BLACKLAB_PASSWORD
+    BLACKLAB_PASSWORD,
+    SHOULD_HAVE_CONTEXT
 };
