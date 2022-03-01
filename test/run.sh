a8b325ebb45a23895b18ac16e1a4328e5e8d7335
Esteban Ginez <175813+eginez@users.noreply.github.com>
2022-03-01 09:30:44 -0800

    Changes to scripts and configurations to allow for integration tests using remote data (#29)
    * Adds data dowloand step
    
    * adds strategies to github actions workflow
    
    * changes name for job integ tests
    
    * Adds documentation for tests

--- test/run.sh
+++ test/run.sh
@@ -6,11 +6,7 @@ export DOCKER_BUILDKIT=1
 
 # Build and run BlackLab Server
 # (--force-recreate to avoid error 'network not found')
-if [ -z ${TOMCAT_APP_NAME}  ]; then
-  docker-compose build testserver
-else
-  docker-compose build --build-arg TOMCAT_APP_NAME=${TOMCAT_APP_NAME} testserver
-fi
+docker-compose build testserver
 
 docker-compose up --force-recreate -d testserver
 
