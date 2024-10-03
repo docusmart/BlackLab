a9dfd5e8ee65803aab1e0088a2ad125e1731dd3a
chunwyu-lexion <163908347+chunwyu-lexion@users.noreply.github.com>
2024-10-03 14:59:18 -0700

    Upgrade AWS SDK and Jackson library (#49)
    * Upgrade AWS SDK and Jackson library
    
    * Push jackson further to the latest
    
    * Update test runner
    
    ---------
    
    Co-authored-by: Chun Yu <Chun.Yu@docusign.com>

--- test/run.sh
+++ test/run.sh
@@ -6,13 +6,13 @@ export DOCKER_BUILDKIT=1
 
 # Build and run BlackLab Server
 # (--force-recreate to avoid error 'network not found')
-docker-compose up --force-recreate -d --build testserver
+docker compose up --force-recreate -d --build testserver
 
 # Build and run the test suite
-docker-compose build test
-docker-compose run --rm test
+docker compose build test
+docker compose run --rm test
 
 # Clean up
 # (stop then down to avoid warning about network in use)
-docker-compose stop testserver
-docker-compose down -v
+docker compose stop testserver
+docker compose down -v
